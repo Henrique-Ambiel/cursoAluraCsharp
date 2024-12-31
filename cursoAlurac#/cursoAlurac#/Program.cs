23de5bef445bda4edc1e98d09b910a17e2a8a65c
Henrique Ambiel <henriqueambiel@gmail.com>
2024-12-31 15:41:14 -0300

    Aula 4

--- cursoAlurac#/cursoAlurac#/Program.cs
+++ cursoAlurac#/cursoAlurac#/Program.cs
@@ -3,7 +3,13 @@ Console.OutputEncoding = Encoding.UTF8;
 
 //Screen Sound
 string messageWelcome = "Bem vindo ao Screen Sound";
-List<string> listBands = new List<string> {"U2", "Bon Jovi", "The Beatles"};
+
+//Registrando bandas e suas 'notas através de listas e dicionários
+//List<string> listBands = new List<string> {"U2", "Bon Jovi", "The Beatles"};
+Dictionary<string, List<int>> dictionaryBands = new Dictionary<string, List<int>>();
+dictionaryBands.Add("Linkin Park", new List<int>{10, 9, 10, 8, 10, 7});
+dictionaryBands.Add("Bon Jovi", new List<int>());
+
 
 //Função de exibir a logo do aplicativo
 void DisplayLogo()
@@ -39,7 +45,7 @@ void DisplayMenuOptions()
             break;
         case 2: ViewBandsRegistered();
             break;
-        case 3: Console.WriteLine("Você digitou a opção: " + selectOption);
+        case 3: EvaluateBand();
             break;
         case 4: Console.WriteLine("Você digitou a opção: " + selectOption);
             break;
@@ -54,14 +60,12 @@ void DisplayMenuOptions()
 void RegisterBand()
 {
     Console.Clear();
-    Console.WriteLine("******************");
-    Console.WriteLine("REGISTRO DE BANDAS");
-    Console.WriteLine("******************");
+    ViewTitleOptions("REGISTRO DE BANDAS");
     Console.Write("\nDigite o nome da banda aqui: ");
     string bandName = Console.ReadLine()!;
-    listBands.Add(bandName);
+    dictionaryBands.Add(bandName, new List<int>());
     Console.WriteLine($"\nA banda {bandName} foi registrada com sucesso");
-    Thread.Sleep(2000);
+    Thread.Sleep(4000);
     Console.Clear();
     DisplayMenuOptions();
 }
@@ -70,14 +74,12 @@ void RegisterBand()
 void ViewBandsRegistered()
 {
     Console.Clear();
-    Console.WriteLine("******************");
-    Console.WriteLine("BANDAS REGISTRADAS");
-    Console.WriteLine("******************\n");
+    ViewTitleOptions("BANDAS REGISTRADAS");
     //for (int i = 0; i < listBands.Count; i++)
     //{
     //    Console.WriteLine($"Banda: {listBands[i]}");
     //}
-    foreach (string band in listBands)
+    foreach (string band in dictionaryBands.Keys)
     {
         Console.WriteLine($"Banda: {band}");
     }
@@ -85,6 +87,43 @@ void ViewBandsRegistered()
     Console.ReadKey();
     Console.Clear();
     DisplayMenuOptions();
+}
+
+//Exibe o título de cada uma das opções
+void ViewTitleOptions(string title)
+{
+   int numberOfLetters = title.Length;
+    string asterisks = string.Empty.PadLeft(numberOfLetters, '*');
+    Console.WriteLine(asterisks);
+    Console.WriteLine(title);
+    Console.WriteLine(asterisks + "\n");
+}
+
+void EvaluateBand()
+{
+    Console.Clear();
+    ViewTitleOptions("AVALIAR BANDA");
+    Console.Write("Digite a banda que você vai avaliar: ");
+    string bandName = Console.ReadLine()!;
+    if (dictionaryBands.ContainsKey(bandName))
+    {
+        Console.Write($"Digite aqui a nota da banda {bandName}: ");
+        int grade = int.Parse(Console.ReadLine()!);
+        dictionaryBands[bandName].Add(grade);
+        Console.WriteLine($"\nNota {grade} registrada com sucesso");
+        Thread.Sleep(2000);
+        Console.Clear();
+        DisplayMenuOptions();
+    }
+    else
+    {
+        Console.WriteLine("Banda não encontrada\n");
+        Console.WriteLine("Digite qualquer tecla para voltar ao menu principal");
+        Console.ReadKey();
+        Console.Clear();
+        DisplayMenuOptions();
+    }
+
 }
 
 DisplayMenuOptions();
